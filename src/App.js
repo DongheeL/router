85809a01ddd0a23715358329d1861be1a9c51f1e
DongheeL <donghlee19@gmail.com>
2023-04-02 23:40:12 +0900

    React-Router 시작

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <p>Home</p>,
+    errorElement: <p>Not Found 😅</p>
+  },
+  {
+    path:'/videos',
+    element: <p>Videos</p>
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
