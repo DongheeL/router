25644b3931f78c1ec011cd1b21e9e22cb59bd93f
DongheeL <donghlee19@gmail.com>
2023-04-04 21:55:25 +0900

    Outlet 사용

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Videos from './pages/Videos';
+import Root from './pages/Root';
 const router = createBrowserRouter([
   {
     path:'/',
-    element: <Home />,
-    errorElement: <NotFound />
-  },
-  {
-    path:'/videos',
-    element: <Videos />
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home />},
+      { path:'/videos', element: <Videos />}
+    ]
   },
 ]);
 
